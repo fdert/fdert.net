68d3f1901525909a6958302fa119e201475c0ce4
fdert <48841998+fdert@users.noreply.github.com>
2026-01-13 11:57:41 +0000

    fix: resolve @ alias and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+<<<<<<< HEAD
+=======
+  base: "/fdert22/",
+>>>>>>> b96d286 (fix: resolve @ alias and vite config)
   plugins: [react(), tsconfigPaths()],
 });
